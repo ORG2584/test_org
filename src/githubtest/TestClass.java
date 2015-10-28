8ee1276ab5d59054160cd2c8135a28fd1fc55c79
xbilya <xbilya@u2-21.cpu.elf.stuba.sk>
2015-10-28 15:44:01 +0100

    vytvorenie master verzie

---
+++ src/githubtest/TestClass.java
@@ -0,0 +1,15 @@
+package githubtest;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author xbilya
+ */
+public class TestClass {
+    
+}
